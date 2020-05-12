8f36064039b7054f1179d4d49b15a1eb4c9f72e8
kyungjun.gil <kyungjun.gil@linecorp.com>
2020-05-12 22:45:41 +0900

    project settings

---
+++ src/main/java/com/marveloper/starter/spring/StarterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.marveloper.starter.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marveloper/starter/spring/StarterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marveloper.starter.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
